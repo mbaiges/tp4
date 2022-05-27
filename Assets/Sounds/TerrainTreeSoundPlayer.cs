99cf20128ea6d6754aef771bdfc724435f2cce19
Matías <mbaiges@itba.edu.ar>
2022-05-27 19:15:18 -0300

    feat: added pitch black skybox and updated terrain sound script

--- Assets/Sounds/TerrainTreeSoundPlayer.cs
+++ Assets/Sounds/TerrainTreeSoundPlayer.cs
@@ -9,16 +9,22 @@ public class TerrainTreeSoundPlayer : MonoBehaviour
 
     // Regular Check
     public float checkEvery;
-    public float spawningRange;
+    public float spawningRangeMin;
+    public float spawningRangeMax;
     public float despawningDistance;
 
-    // Crickets
-    public AudioClip[] cricketSounds;
-    public float cricketTimeBetweenMin = 0;
-    public float cricketTimeBetweenMax = 5;
-    public float cricketVolumeMin = 0.8f;
-    public float cricketVolumeMax = 1;
-    public int cricketsMaxNearPlayer = 4;
+    // Sounds
+    public AudioClip[] sounds;
+    public int maxNearPlayer = 4;
+    public int maxInMap = 100;
+    public float timeBetweenMin = 0;
+    public float timeBetweenMax = 5;
+    public float pitchMin = 0.8f;
+    public float pitchMax = 1;
+    public float volumeMin = 0.8f;
+    public float volumeMax = 1;
+    public float treeHeightMin = 0;
+    public float treeHeightMax = 0;
 
     // Trees
     private TreeInstance[] trees;
@@ -28,8 +34,8 @@ public class TerrainTreeSoundPlayer : MonoBehaviour
     private List<TreeInstance> closestTrees = new List<TreeInstance>();
 
     // Crickets
-    private List<GameObject> cricketAll = new List<GameObject>();
-    private float cricketNextAt;
+    private List<GameObject> all = new List<GameObject>();
+    private float nextAt;
 
     // Regular Check
     private float checkNextAt;
@@ -46,7 +52,7 @@ public class TerrainTreeSoundPlayer : MonoBehaviour
         elapsedTime = 0;
         playerPosition = player.transform.position;
         checkNextAt = startTime;
-        cricketNextAt = cricketTimeBetweenMax; // We start with max time
+        nextAt = timeBetweenMax; // We start with max time
         InitTrees();
         FindClosestTrees();
     }
@@ -69,7 +75,8 @@ public class TerrainTreeSoundPlayer : MonoBehaviour
 
             foreach (TreeInstance tree in trees) {
                 Vector3 position = GetTreeBasePosition(tree);
-                if (Vector3.Distance(position, playerPosition) < spawningRange) {
+                float distance = Vector3.Distance(position, playerPosition);
+                if (distance >= spawningRangeMin && distance <= spawningRangeMax) {
                     closest.Add(tree);
                 }
             }
@@ -82,7 +89,9 @@ public class TerrainTreeSoundPlayer : MonoBehaviour
     private List<GameObject> NearSoundDatas(List<GameObject> datas) {
         List<GameObject> nearest = new List<GameObject>();
         foreach (GameObject data in datas) {
-            if (Vector3.Distance(data.transform.position, playerPosition) < spawningRange) {
+            // y fixed to player pos (instead of sphere around player, its a cylinder)
+            float distance = Vector3.Distance(new Vector3(data.transform.position.x, playerPosition.y, data.transform.position.z), playerPosition);
+            if (distance <= spawningRangeMax) {
                 nearest.Add(data);
             }
         }
@@ -90,23 +99,26 @@ public class TerrainTreeSoundPlayer : MonoBehaviour
     }
 
     private float SpawnSoundOnTreeBase(AudioClip[] sources, List<GameObject> all, List<GameObject> nearestSpawned, int maxNearPlayer, float nextAt, float timeBetweenMin, float timeBetweenMax) {
-        if (closestTrees.Count > 0 && nearestSpawned.Count < maxNearPlayer && elapsedTime >= nextAt) {
+        if (closestTrees.Count > 0 && all.Count < maxInMap && nearestSpawned.Count < maxNearPlayer && elapsedTime >= nextAt) {
             // Select a tree nearby
             TreeInstance tree = closestTrees[(int) Random.Range(0, closestTrees.Count-1)];
             Vector3 position = GetTreeBasePosition(tree);
+            position.y += Random.Range(treeHeightMin, treeHeightMax);
 
             // Instantiate sound
-            GameObject soundSource = new GameObject("Temporal Sound Source");
+            AudioClip clip = sounds[(int) Random.Range(0, sounds.Length-1)];
+            GameObject soundSource = new GameObject(clip.name);
             // the new gameobject to be a child of the gameobject that your script is attached to
             soundSource.transform.parent = this.gameObject.transform;
             soundSource.transform.position = position;
             AudioSource src = soundSource.AddComponent<AudioSource>();
-            src.clip = cricketSounds[(int) Random.Range(0, cricketSounds.Length-1)];
-            src.volume = Random.Range(cricketVolumeMin, cricketVolumeMax);
-            src.pitch = Random.Range(cricketVolumeMin, cricketVolumeMax);
+            src.clip = sounds[(int) Random.Range(0, sounds.Length-1)];
+            src.volume = Random.Range(volumeMin, volumeMax);
+            src.pitch = Random.Range(pitchMin, pitchMax);
             src.spatialize = true;
             src.spatializePostEffects = true;
             src.spatialBlend = 1;
+            // src.rolloffMode = AudioRolloffMode.Linear;
             src.Play();
             all.Add(soundSource);
 
@@ -128,13 +140,13 @@ public class TerrainTreeSoundPlayer : MonoBehaviour
         }
     }
 
-    private void HandleCrickets() {
-        float nextAt = SpawnSoundOnTreeBase(cricketSounds, cricketAll, NearSoundDatas(cricketAll), cricketsMaxNearPlayer, cricketNextAt, cricketTimeBetweenMin, cricketTimeBetweenMax);
+    private void HandleSounds() {
+        float nextAt = SpawnSoundOnTreeBase(sounds, all, NearSoundDatas(all), maxNearPlayer, this.nextAt, timeBetweenMin, timeBetweenMax);
         if (nextAt > 0) { // If we spawned a sound
             // Delete far enough creeks
-            DeleteFarEnoughSounds(cricketAll);
+            DeleteFarEnoughSounds(all);
             // Calculate next cricket appearance time
-            cricketNextAt = nextAt;
+            this.nextAt = nextAt;
         }
     }
 
@@ -142,6 +154,6 @@ public class TerrainTreeSoundPlayer : MonoBehaviour
         elapsedTime = Time.time - startTime;
         playerPosition = player.transform.position;
         FindClosestTrees();
-        HandleCrickets();
+        HandleSounds();
     }
 }
