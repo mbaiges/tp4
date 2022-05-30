3d7d0dc144cde0c64e268be7dade62cfcb509563
GMargo <gmargossian@itba.edu.ar>
2022-05-30 19:55:39 -0300

    add walkman

---
+++ Assets/Prefabs/Walkman/credits.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class credits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Walkman Credits to https://www.cgtrader.com/items/2839865/download-page
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
