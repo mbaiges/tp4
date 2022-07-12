7b66dd4b9a9e0e5b17dbbd58722607b4aaae5ce8
GMargo <gmargossian@itba.edu.ar>
2022-07-12 15:41:46 -0300

    add walkman beeps

--- Assets/Scripts/WalkmanPlayer.cs
+++ Assets/Scripts/WalkmanPlayer.cs
@@ -5,9 +5,11 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class WalkmanPlayer : MonoBehaviour
 {
     public AudioSource sound = null;
+    public AudioSource beeps = null;
     private XRGrabInteractable grabInteractable = null;
     public void onActivate() {
         sound.Play();
+        beeps.Stop();
     }
 
 }
